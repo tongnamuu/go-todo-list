2df55a45ae1f70bda1af02cffde08c0943d5dc95
tongnamuu <thban33@naver.com>
2023-11-17 14:06:39 +0900

    feature: WebHandler 추가

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	mux2 "github.com/gorilla/mux"
+	"net/http"
+)
+
+func MakeWebHandler() http.Handler {
+	mux := mux2.NewRouter()
+	mux.Handle("/", http.FileServer(http.Dir("public")))
+	mux.HandleFunc("/todos", GetTodoListHandler).Methods("GET")
+	mux.HandleFunc("/todos", PostTodoListHandler).Methods("POST")
+	mux.HandleFunc("/todos/{id:[0-9]+}", DeleteTodoListHandler).Methods("DELETE")
+	mux.HandleFunc("/todos/{id:[0-9]+}", UpdateTodoListHandler).Methods("PUT")
+	return mux
+}
+
+func UpdateTodoListHandler(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func DeleteTodoListHandler(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func PostTodoListHandler(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func GetTodoListHandler(writer http.ResponseWriter, request *http.Request) {
+
+}
 
 func main() {
-	fmt.Println("main")
+
 }
