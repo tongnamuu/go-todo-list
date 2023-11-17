1d8aa22519dbfe4835762805b1060d9d758fe804
tongnamuu <thban33@naver.com>
2023-11-17 18:35:32 +0900

    feature: DeleteTodoListHandler, UpdateTodoListHandler

--- main.go
+++ main.go
@@ -7,9 +7,14 @@ import (
 	"log"
 	"net/http"
 	"sort"
+	"strconv"
 )
 
 var rd *render.Render
+
+type Success struct {
+	Success bool `json:"success"`
+}
 
 func MakeWebHandler() http.Handler {
 	mux := mux2.NewRouter()
@@ -22,11 +27,34 @@ func MakeWebHandler() http.Handler {
 }
 
 func UpdateTodoListHandler(writer http.ResponseWriter, request *http.Request) {
+	var newTodo Todo
+	err := json.NewDecoder(request.Body).Decode(&newTodo)
+	if err != nil {
+		log.Fatal(err)
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
+	vars := mux2.Vars(request)
+	id, _ := strconv.Atoi(vars["id"])
+	if todo, ok := todoMap[id]; ok {
+		todo.Name = newTodo.Name
+		todo.Completed = newTodo.Completed
+		rd.JSON(writer, http.StatusOK, Success{true})
+		return
+	}
+	rd.JSON(writer, http.StatusNotFound, Success{false})
 }
 
 func DeleteTodoListHandler(writer http.ResponseWriter, request *http.Request) {
-
+	vars := mux2.Vars(request)
+	id, _ := strconv.Atoi(vars["id"])
+	if _, ok := todoMap[id]; ok {
+		delete(todoMap, id)
+		rd.JSON(writer, http.StatusOK, Success{true})
+		return
+	}
+	rd.JSON(writer, http.StatusNotFound, Success{false})
 }
 
 func PostTodoListHandler(writer http.ResponseWriter, request *http.Request) {
